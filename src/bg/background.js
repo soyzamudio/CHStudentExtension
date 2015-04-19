599b281663ecbf68ab3da77d3d8313db2aa04f26
Jose Zamudio <zamudio@outlook.com>
2015-04-19 14:43:43 -0700

    Change icon for food

--- src/bg/background.js
+++ src/bg/background.js
@@ -7,7 +7,7 @@ angular.module('background', ['firebase'])
       type: "basic",
       title: 'Dinner is ready!',
       message: snapshot.val().dinner,
-      iconUrl: "../../icons/icon128.png"
+      iconUrl: "../../icons/food.png"
     }
     var current = new Date().getTime();
     (current - 600000 > snapshot.val().timestamp) ? console.log('Old notification') : chrome.notifications.create('notify' + i, opt, function(id) { i++; });
